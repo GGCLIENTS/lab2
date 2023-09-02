d7b19548043c92e894cf3551e8981b239e03ca43
GGCLIENT <137526018+GGCLIENTS@users.noreply.github.com>
2023-09-02 07:41:12 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+
+var  Number = +prompt("World war 1 heden ond bolson be")
+if (Number == 1914) {
+    alert("Correct")
+} else {
+    alert("worng answer")
+}
+var  Number = +prompt("bi heden nastai we?")
+if (Number == 14) {
+    alert("Correct")
+} else {
+    alert("worng answer")
+}
+var  Number = +prompt("muur heden hultui we")
+if (Number == 4) {
+    alert("Correct")
+} else {
+    alert("worng answer")
+}
+var  Number = +prompt("1jild heden honog bdag we?")
+if (Number == 365) {
+    alert("Correct")
+} else {
+    alert("worng answer")
+}
+var  Number = +prompt("tsagt heden sec bdag we?")
+if (Number == 3600) {
+    alert("Correct")
+} else {
+    alert("worng answer")
+}
